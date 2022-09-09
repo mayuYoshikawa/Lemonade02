4ce3f94d74ae7e0e492d6a8ffddcffb594d0b655
mayu <m.yoshikawa25@gmail.com>
2022-09-09 16:25:15 +0900

    comit

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -17,35 +17,34 @@ package com.example.lemonade
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    /**
-     * DO NOT ALTER ANY VARIABLE OR VALUE NAMES OR THEIR INITIAL VALUES.
-     *
-     * Anything labeled var instead of val is expected to be changed in the functions but DO NOT
-     * alter their initial values declared here, this could cause the app to not function properly.
-     */
     private val LEMONADE_STATE = "LEMONADE_STATE"
     private val LEMON_SIZE = "LEMON_SIZE"
     private val SQUEEZE_COUNT = "SQUEEZE_COUNT"
-    // SELECT represents the "pick lemon" state
+
+    // レモンを選ぶ状態
     private val SELECT = "select"
-    // SQUEEZE represents the "squeeze lemon" state
+    // レモンを搾る状態
     private val SQUEEZE = "squeeze"
-    // DRINK represents the "drink lemonade" state
+    // レモネードを飲む状態
     private val DRINK = "drink"
-    // RESTART represents the state where the lemonade has been drunk and the glass is empty
+    // グラスが空の状態
     private val RESTART = "restart"
-    // Default the state to select
+    // 選択されている状態
     private var lemonadeState = "select"
-    // Default lemonSize to -1
+    // ランダムな数字を格納する為の変数
     private var lemonSize = -1
-    // Default the squeezeCount to -1
+    // レモンを搾った回数
     private var squeezeCount = -1
+    // レモネードを何回作ったか
+    private var lemonadeCount = 0
 
     private var lemonTree = LemonTree()
     private var lemonImage: ImageView? = null
@@ -62,21 +61,38 @@ class MainActivity : AppCompatActivity() {
             squeezeCount = savedInstanceState.getInt(SQUEEZE_COUNT, -1)
         }
         // === END IF STATEMENT ===
-
         lemonImage = findViewById(R.id.image_lemon_state)
         setViewElements()
+
+        // 画像がクリックされたら、メソッドを呼び出す
         lemonImage!!.setOnClickListener {
-            // TODO: call the method that handles the state when the image is clicked
+            clickLemonImage()
         }
+        // 画像が長押しされたらメソッドを呼び出す
         lemonImage!!.setOnLongClickListener {
-            // TODO: replace 'false' with a call to the function that shows the squeeze count
+            showSnackbar()
             false
+        }
+
+        // ボタンのオブジェクト参照のため、変数に格納
+        var countButton = findViewById<Button>(R.id.CountButton)
+        var skipButton = findViewById<Button>(R.id.SkipButton)
+
+        //カウントボタンがクリックされたらテキストを表示させる
+        countButton.setOnClickListener {
+            val countText: TextView = findViewById(R.id.countText)
+            countText.text = "${lemonadeCount}回飲んだよ"
+        }
+        //skipボタンがクリックされたらDRINKの画面に遷移する
+        skipButton.setOnClickListener {
+            lemonSize = 1
+            lemonadeState = SQUEEZE
+            clickLemonImage()
         }
     }
 
     /**
      * === DO NOT ALTER THIS METHOD ===
-     *
      * This method saves the state of the app if it is put in the background.
      */
     override fun onSaveInstanceState(outState: Bundle) {
@@ -86,51 +102,69 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
     }
 
-    /**
-     * Clicking will elicit a different response depending on the state.
-     * This method determines the state and proceeds with the correct action.
-     */
     private fun clickLemonImage() {
-        // TODO: use a conditional statement like 'if' or 'when' to track the lemonadeState
-        //  when the image is clicked we may need to change state to the next step in the
-        //  lemonade making progression (or at least make some changes to the current state in the
-        //  case of squeezing the lemon). That should be done in this conditional statement
 
-        // TODO: When the image is clicked in the SELECT state, the state should become SQUEEZE
-        //  - The lemonSize variable needs to be set using the 'pick()' method in the LemonTree class
-        //  - The squeezeCount should be 0 since we haven't squeezed any lemons just yet.
+        when (lemonadeState) {
 
-        // TODO: When the image is clicked in the SQUEEZE state the squeezeCount needs to be
-        //  INCREASED by 1 and lemonSize needs to be DECREASED by 1.
-        //  - If the lemonSize has reached 0, it has been juiced and the state should become DRINK
-        //  - Additionally, lemonSize is no longer relevant and should be set to -1
-
-        // TODO: When the image is clicked in the DRINK state the state should become RESTART
-
-        // TODO: When the image is clicked in the RESTART state the state should become SELECT
-
-        // TODO: lastly, before the function terminates we need to set the view elements so that the
-        //  UI can reflect the correct state
+            //ランダムな数字をlemonSizeに格納、レモン搾った回数をリセットし、
+            //lemonadeStateを更新する
+            SELECT -> {
+                lemonSize = lemonTree.pick()
+                squeezeCount = 0
+                lemonadeState = SQUEEZE
+            }
+            //レモンを絞った回数(squeezeCount)を増やし、ランダムな数字(lemonSize)を減らす
+            SQUEEZE -> {
+                squeezeCount++
+                lemonSize--
+                //lemonSizeが0の場合のみlemonadeStateを更新
+                if (lemonSize == 0) {
+                    lemonadeState = DRINK
+                }
+            }
+            //lemonSizeをリセット、何回飲んだかのlemonadeCountを増やし、lemonadeStateを更新
+            DRINK -> {
+                lemonSize = -1
+                lemonadeCount++
+                lemonadeState = RESTART
+            }
+            //lemonadeStateを更新
+            RESTART -> {
+                lemonadeState = SELECT
+            }
+        }
+        //メソッド呼び出す
+        setViewElements()
     }
 
-    /**
-     * Set up the view elements according to the state.
-     */
     private fun setViewElements() {
+        //テキストと画像のオブジェクト参照のため変数に格納
         val textAction: TextView = findViewById(R.id.text_action)
-        // TODO: set up a conditional that tracks the lemonadeState
+        val lemonImage: ImageView = findViewById(R.id.image_lemon_state)
 
-        // TODO: for each state, the textAction TextView should be set to the corresponding string from
-        //  the string resources file. The strings are named to match the state
-
-        // TODO: Additionally, for each state, the lemonImage should be set to the corresponding
-        //  drawable from the drawable resources. The drawables have the same names as the strings
-        //  but remember that they are drawables, not strings.
+        //lemonadeStateに対応した文字と画像を表示する
+        when (lemonadeState) {
+            SELECT -> {
+                textAction.setText(R.string.lemon_select)
+                lemonImage.setImageResource(R.drawable.lemon_tree)
+            }
+            SQUEEZE -> {
+                textAction.setText(R.string.lemon_squeeze)
+                lemonImage.setImageResource(R.drawable.lemon_squeeze)
+            }
+            DRINK -> {
+                textAction.setText(R.string.lemon_drink)
+                lemonImage.setImageResource(R.drawable.lemon_drink)
+            }
+            RESTART -> {
+                textAction.setText(R.string.lemon_empty_glass)
+                lemonImage.setImageResource(R.drawable.lemon_restart)
+            }
+        }
     }
 
     /**
      * === DO NOT ALTER THIS METHOD ===
-     *
      * Long clicking the lemon image will show how many times the lemon has been squeezed.
      */
     private fun showSnackbar(): Boolean {
@@ -147,12 +181,10 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-/**
- * A Lemon tree class with a method to "pick" a lemon. The "size" of the lemon is randomized
- * and determines how many times a lemon needs to be squeezed before you get lemonade.
- */
+//ランダムな数字生成
 class LemonTree {
     fun pick(): Int {
         return (2..4).random()
     }
 }
+
